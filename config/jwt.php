c96a0504867c81a34e23cd5412d541bc7a76ae91
Spartan273 <sami273@live.ca>
2017-09-30 13:33:14 -0400

    generation secret key

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', '2FjMZgCy7S7hZieJ7jCvdbXAdKfkzKV8'),
 
     /*
     |--------------------------------------------------------------------------
